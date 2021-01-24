8d2543973ad37f7f183a2532db09d4c2b8d908aa
VaradMash <VaradMash@users.noreply.github.com>
2021-01-24 15:41:08 +0530

    Challenge 1(Plane Controller) added.

---
+++ Challenge 1/Assets/Challenge 1/Scripts/propellerRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class propellerRotation : MonoBehaviour
+{
+    public float rotation_speed = 15.0f;
+    public float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        Vector3 rotation_vector = new Vector3(0,0,1);
+        transform.Rotate(rotation_vector * rotation_speed);
+    }
+}
