5a1674872a31d6617de5b6cacc97219ec68ca202
VaradMash <VaradMash@users.noreply.github.com>
2021-02-06 17:32:43 +0530

    Prototype 2 Project(Feed the Animals) added.

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed=10.0f;
+    public float xRange = 10.0f;
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get user input for horizontal axis.
+        horizontalInput = Input.GetAxis("Horizontal");
+        //relocate position of the player if player tries to move out of bounds.
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a food projectile
+            //Creates a clone of the gameobject in consideration.
+            Instantiate(projectilePrefab, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawn_x_Range = 10.0f;
+    private float spawn_z_position = 20.0f;
+    private float start_delay = 2.0f;
+    private float spawn_interval = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawn_random_animal", start_delay, spawn_interval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawn_random_animal()
+    {
+        int index = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawn_x_Range, spawn_x_Range), 0, spawn_z_position);
+        Instantiate(animalPrefabs[index], spawnPosition, animalPrefabs[index].transform.rotation);
+    }
+
+}
